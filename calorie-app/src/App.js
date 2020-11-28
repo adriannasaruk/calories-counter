368ce8b107e2b848e139bf2c4d5b2d73d188cff6
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-11-27 18:09:20 -1000

    project

---
+++ calorie-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Header from "./components/header"
+import Home from "./components/home"
+import Footer from "./components/footer"
+
+export default function App() {
+  return (
+    <body>
+      <div class="body">
+        <Home/>
+        <Footer/>
+      </div>
+    </body>
+  );
+}
